6c708a122237318b19ec3a09d228cd33e0a1daed
Ivarock <ivarssluka@gmail.com>
2024-10-06 13:29:40 +0300

    Null data type

--- index.php
+++ index.php
@@ -1,76 +1,46 @@
 <?php
 
-/* Strings */
+/* NULL */
 
-$firstName = 'John'; // You cannot use variables in single quotes
-$lastName = 'Doe';
-$fullName = "$firstName Doe"; // You can use variables in double quotes
-echo $firstName . '<br>';
-echo $fullName . '<br>';
-$fullName = "{$firstName} Doe"; // You can use variables in double quotes
-echo $fullName . '<br>';
-//$fullName = "${firstName} Doe"; // This is Deprecated
-//echo $fullName . '<br>';
+// null constant
 
-$fullName = "$firstName $lastName"; // You can use variables in double quotes
-echo $fullName . '<br>';
-$fullName = $firstName . ' ' . $lastName; // String concatenation
-echo $fullName . '<br>';
+var_dump($x); // Prints NULL with an error
+echo '<br>';
+var_dump(is_null($x)); // Prints boolean true with an error
 
-echo $fullName[0] . '<br>'; // Get the first character
-echo $fullName[-2] . '<br>'; // Get the second character from the end
-echo strlen($fullName) . '<br>'; // Get the length of the string
-$fullName[1] = 'I'; // Change the second character
-echo $fullName . '<br>';
-var_dump($fullName);
+$x = null;
+echo $x . '<br>'; // When echoing null, it prints nothing because it gets converted to an empty string
+var_dump($x); // Prints NULL
+echo '<br>';
+var_dump(is_null($x)); // Prints boolean true
 echo '<br>';
 
-$fullName[20] = 'X'; // Add an extra character
-var_dump($fullName); // This will add an extra character to the string an fill the rest with spaces
+$x = 212;
+var_dump(is_null($x)); // Prints boolean false
 echo '<br>';
 
-// Heredoc treats the text as if it were in double quotes
-
-$x = 10;
-$y = 20;
-$text = <<<TEXT
-Line 1 $x
-Line two $y
-Line three ' " 
-TEXT;
-echo $text; // Output: Line 1 Line two Line three
+$x = null;
+var_dump($x === null); // Prints boolean true
 echo '<br>';
-echo nl2br($text); // Output: Line 1<br />Line two<br />Line three
-
-// Nowdoc treats the text as if it were in single quotes
-$text = <<<'TEXT'
-Line 1 $x
-Line two $y
-Line three ' "
-TEXT;
-// Only difference is that you enclose the text in single quotes
-echo $text; // Output: Line 1 $x Line two $y Line three
-echo '<br>';
-echo nl2br($text); // Output: Line 1<br />Line two $y <br />Line three // Variable values will not be displayed.
-// Also, you don't need escape symbols with this syntax
-
-$text = <<<TEXT
-<div>
-<p>Hello</p>
-<p>World</p>
-</div>
-TEXT;
-echo $text; // You can have full html tags here.
+var_dump($x !== null); // Prints boolean false
 echo '<br>';
 
-$text = <<<TEXT
-Hello World
-TEXT;
-var_dump($text); // Output: string(11) "Hello World"
+$x = 0000;
+var_dump($x);
+echo '<br>';
+unset ($x); // Unsets the value of $x
+var_dump($x); // Prints an error and returns NULL
 echo '<br>';
 
-$text = <<<TEXT
-    Hello World
-TEXT;
-var_dump($text); // Output: string(15) "Hello World". It counts the spaces as well.
+// Type Casting
+$x = null;
+var_dump((string)$x); // Prints an empty string
+echo '<br>';
+var_dump((int)$x); // Prints 0
+echo '<br>';
+var_dump((bool)$x); // Prints boolean false
+echo '<br>';
+var_dump((double)$x); // Prints float 0
+echo '<br>';
+var_dump((array)$x); // Prints an empty array
 echo '<br>';
